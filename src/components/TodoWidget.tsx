d303c634ff92cede71f719300e558d589fa65f40
Nikita Kisiura <nikitar156@gmail.com>
2024-11-27 09:57:24 -0500

    create use-todos hook

--- src/components/TodoWidget.tsx
+++ src/components/TodoWidget.tsx
@@ -1,6 +1,4 @@
-import { useReducer } from "react";
 import { clsx } from "clsx";
-import { todosReducer } from "../reducers/todosReducer";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
@@ -10,13 +8,19 @@ import {
   isCompletedTodo,
   isIncompleteTodo,
 } from "../helpers/todo-helpers";
-import { TodoFilter } from "../types/todo-filter";
+import { useTodos } from "../hooks/useTodos";
 
 export const TodoWidget = () => {
-  const [{ todos, filter }, dispatch] = useReducer(todosReducer, {
-    todos: [],
-    filter: "all",
-  });
+  const {
+    todosState: { todos, filter },
+    handleAddTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+    handleEditTodo,
+    handleClearCompletedTodos,
+    handleFilterChange,
+    handleToggleAllTodosByFilter,
+  } = useTodos({ todos: [], filter: "all" });
 
   const todosByFilter = getTodosByFilter(todos, filter);
   const isAllTodosByFilterCompleted = todosByFilter.every(isCompletedTodo);
@@ -25,40 +29,6 @@ export const TodoWidget = () => {
   const isToggleAllButtonVisible = !!todosByFilter.length;
   const isTodoListVisible = !!todosByFilter.length;
   const isFooterVisible = !!todos.length;
-
-  const handleAddTodo = (todo: string) => {
-    dispatch({ type: "added", payload: { todo } });
-  };
-
-  const handleDeleteTodo = (todoId: string) => {
-    dispatch({ type: "deleted", payload: { todoId } });
-  };
-
-  const handleToggleTodo = (todoId: string) => {
-    dispatch({ type: "toggled", payload: { todoId } });
-  };
-
-  const handleEditTodo = (todoId: string, updatedTodo: string) => {
-    dispatch({ type: "edited", payload: { todoId, updatedTodo } });
-  };
-
-  const handleClearCompletedTodos = () => {
-    dispatch({ type: "cleared-completed", payload: {} });
-  };
-
-  const handleToggleAllTodosByFilter = () => {
-    const nextStatus = !isAllTodosByFilterCompleted;
-    const todoIds = todosByFilter.map(({ id }) => id);
-
-    dispatch({
-      type: "toggled-all-by-filter",
-      payload: { nextStatus, todoIds },
-    });
-  };
-
-  const handleFilterChange = (filter: TodoFilter) => {
-    dispatch({ type: "filter-changed", payload: { filter } });
-  };
 
   return (
     <div className="flex w-full max-w-[34rem] flex-col gap-3">
@@ -70,7 +40,9 @@ export const TodoWidget = () => {
               "px-2 py-3 text-xl focus:outline-0",
               isAllTodosByFilterCompleted ? "text-teal-600" : "text-teal-400",
             )}
-            onClick={handleToggleAllTodosByFilter}
+            onClick={() => {
+              handleToggleAllTodosByFilter(todosByFilter);
+            }}
           >
             <i className="fa-solid fa-chevron-down"></i>
           </button>
